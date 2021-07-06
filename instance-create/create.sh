7b556c4d2e1ce0427e27331c624cf933ed52fb25
santhu <devopsaws.santhosh@gmail.com>
2021-07-06 09:16:59 +0530

    chaneged

--- instance-create/create.sh
+++ instance-create/create.sh
@@ -24,7 +24,7 @@ Instance_Create() {
   IPADDRESS=$(aws ec2 describe-instances     --filters Name=tag:Name,Values=${COMPONENT}   | jq .Reservations[].Instances[].PrivateIpAddress | grep -v null |xargs)
 
   sed -e "s/COMPONENT/${COMPONENT}/" -e "s/IPADDRESS/${IPADDRESS}/" record.json >/tmp/record.json
-  aws route53 change-resource-record-sets --hosted-zone-id Z0449177GO3AQK4VZZ1H --change-batch file:///tmp/record.json
+  aws route53 change-resource-record-sets --hosted-zone-id Z02175683ALN238DUJRLK --change-batch file:///tmp/record.json
   sed -i -e "/${COMPONENT}/ d" ../inventory
   echo "${IPADDRESS} APP=$(echo ${COMPONENT} | awk -F - '{print $1}')" >>../inventory
 }
